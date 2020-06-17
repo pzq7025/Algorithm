a594469cd98f54e545bde808036438f0d7ea3f8f
Pzq <37291799+pzq7025@users.noreply.github.com>
2020-06-17 13:06:23 +0800

    Create spare_matrix.py
    leetcode17.26 question

---
+++ spare_matrix.py
@@ -0,0 +1,25 @@
+matrixx_ = [
+    [14, 15, 100, 9, 3],
+    [32, 1, 9, 3, 5],
+    [15, 29, 2, 6, 8, 7],
+    [7, 10]
+]
+def spare_(matrix):
+    matrix = [set(i) for i in matrix]
+    final_result = []
+    for i in range(len(matrix)):
+        for k in range(i + 1, len(matrix)):
+            # 如果有交集才进行并集运算
+            union = matrix[i] & matrix[k]
+            if len(union) > 0:
+                combine = matrix[i] | matrix[k]
+                # 这里需要注意一下精度的问题
+                num_detail = int(len(union) / len(combine) * 10 ** 5)
+                if num_detail % 10 >= 5:
+                    num_detail = (num_detail // 10 * 10 + 10) / (10 ** 5)
+                else:
+                    num_detail = num_detail / (10 ** 5)
+                result = f"{i},{k}: {num_detail:.4f}"
+                final_result.append(result)
+    print(final_result)
+spare_(matrix_)
