406cdc6ccf6be6c19e6242713f050bdbe5088840
Pzq <37291799+pzq7025@users.noreply.github.com>
2020-12-17 18:17:56 +0800

    Create tree.h

---
+++ treeOp/tree.h
@@ -0,0 +1,13 @@
+//
+// Created by 14276 on 2020/8/20.
+//
+
+#ifndef FIRST_TREE_H
+#define FIRST_TREE_H
+struct Tree {
+    int data{};
+    Tree *rightTree = nullptr;
+    Tree *leftTree = nullptr;
+};
+
+#endif //FIRST_TREE_H
